84846482a8f5e63e8eace36757519bc41a32a7cd
SPIN DSpinddD <spindd@SPINnoMacBook-Pro-2.local>
2021-04-05 13:19:55 +0900

    add build file

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,18 +1,18 @@
 var plugins = [{
       name: 'gatsby-plugin-react-helmet',
-      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shpify/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shopify/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-layout',
-      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shpify/node_modules/gatsby-plugin-layout/gatsby-ssr'),
+      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shopify/node_modules/gatsby-plugin-layout/gatsby-ssr'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-manifest',
-      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shpify/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shopify/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
     },{
       name: 'gatsby-plugin-google-analytics',
-      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shpify/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
+      plugin: require('/Users/spindd/Desktop/spin1/gatsby_shopify/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
       options: {"plugins":[],"trackingId":"UA-134421805-1","anonymize":true,"respectDNT":true,"head":false,"exclude":[],"pageTransitionDelay":0},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,10 +4,10 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---src-pages-404-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shpify/src/pages/404.js")),
-  "component---src-pages-cart-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shpify/src/pages/cart.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shpify/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shpify/src/pages/page-2.js")),
-  "component---src-templates-product-page-index-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shpify/src/templates/ProductPage/index.js"))
+  "component---src-pages-404-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shopify/src/pages/404.js")),
+  "component---src-pages-cart-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shopify/src/pages/cart.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shopify/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shopify/src/pages/page-2.js")),
+  "component---src-templates-product-page-index-js": preferDefault(require("/Users/spindd/Desktop/spin1/gatsby_shopify/src/templates/ProductPage/index.js"))
 }
 
